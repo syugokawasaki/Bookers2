afdceb8e337a186721479d08f26b27496486100b
syugokawasaki <n.m.raminas@gmail.com>
2021-07-21 09:49:32 +0000

    [ADD] 表示させるViewを作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "homes#top"
+  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update,]
+  resources :users, only: [:index, :show, :edit, :update]
 
 end
